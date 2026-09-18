fb4d4b7a3a6b29120c73e360017cd2e571779faa
Mavuso1 <st10478373@rcconnect.edu.za>
2026-09-18 14:18:40 +0200

    commit

--- Login.java
+++ Login.java
@@ -1,3 +1,23 @@
 public class Login {
 
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+
+    public Login(String firstName, String lastName, String username, String password, String cellPhoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+}
+public Boolean checkUserName() {
+    if (username.contains("_") && username.length() <= 5) {
+        return true;
+    } else {
+        return false;
+    }
 }
